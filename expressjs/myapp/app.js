b648c8292a95afa896784b619af654d6fbd17aaa
Zain Saleem <zain.saleem3@ontariotechu.net>
2023-11-02 18:41:55 -0400

    init

--- expressjs/myapp/app.js
+++ expressjs/myapp/app.js
@@ -40,7 +40,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('./views/error.js');
+  res.render('error');
 });
 
 module.exports = app;
