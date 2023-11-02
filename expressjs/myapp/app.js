be6d77b5eb87dbc5a2828c74e364677c4e27f2ad
Zain Saleem <zain.saleem3@ontariotechu.net>
2023-11-02 18:33:13 -0400

    init

--- expressjs/myapp/app.js
+++ expressjs/myapp/app.js
@@ -14,7 +14,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
